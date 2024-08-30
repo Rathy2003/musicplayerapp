2888fd6ec2b65630cede2b94b6da51f6d62ca072
rathy2303 <thyjks2020@gmail.com>
2024-08-30 12:19:49 +0700

    Fix Error On IOS

--- lib/screen/home_screen_page.dart
+++ lib/screen/home_screen_page.dart
@@ -13,111 +13,15 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("My Playlist"),
       ),
-      body: Column(
-        children: [
-          // Playlist
-          Expanded(
-            child: GetBuilder<HomeScreenController>(
-              init: HomeScreenController(),
-              builder: (controller) {
-                return ListView.builder(
-                  itemCount: controller.songsData.length,
-                  itemBuilder: (context, index) {
-                    return customTileList(
-                        title:controller.songsData[index]["title"],
-                        artist:controller.songsData[index]["artist"],
-                        cover:controller.songsData[index]["cover"],
-                        onTap:(){
-                          controller.onTapOnSongList(index);
-                        },
-                        isActive: controller.currentSongInd == index ? true : false,
-                        isPause: controller.isPlay
-                    );
-                  },);
-              }
-            ),
-          ),
-
-          // Audio Player
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 18,vertical: 10),
-            decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.7),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: const Offset(0, 3), // changes position of shadow
-                  ),
-                ],
-                borderRadius: const BorderRadius.only(topRight: Radius.circular(15),topLeft: Radius.circular(15))
-            ),
-            child: GetBuilder<HomeScreenController>(
-              init: HomeScreenController(),
-              builder: (controller) {
-                return Column(
-                  children: [
-                    Slider.adaptive(
-                      value: controller.position.inSeconds.toDouble(),
-                      min: 0.0,
-                      max: controller.duration.inSeconds.toDouble(),
-                      onChanged: (val)=>{
-                      },
-                      activeColor: Colors.orange,
-                    ),
-                    Row(
-                      children: [
-                        Container(
-                            height: 70,
-                            width: 70,
-                            clipBehavior: Clip.hardEdge,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8),
-                                color: Colors.grey.withOpacity(0.5)
-                            ),
-                            child: controller.currentSong==""?Image.asset("assets/images/disc.png") : Image.network(controller.currentCover,fit: BoxFit.cover,)
-                        ),
-                        const SizedBox(width: 10,),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width / 1.90,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(controller.currentTitle,style:
-                              const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w600
-                              ),
-                                overflow: TextOverflow.fade,
-                                maxLines: 1,
-                                softWrap: false,
-                              ),
-                              Text(controller.currentArtist,
-                                style: const TextStyle(
-                                  fontSize: 17,
-                                ),
-                                overflow: TextOverflow.fade,
-                                maxLines: 1,
-                                softWrap: false,
-                              )
-                            ],
-                          ),
-                        ),
-                        IconButton(
-                            onPressed: (){
-                                controller.onPressPlayOrPauseButton();
-                            },
-                            icon:Icon(controller.isPlay?Icons.pause:controller.icon,size: 50,)
-                        )
-                      ],
-                    )
-                  ],
-                );
-              }
-            ),
-          )
-        ],
+      body: Center(
+        child: GetBuilder<HomeScreenController>(
+          init: HomeScreenController(),
+          builder: (controller) {
+            return Text(
+              controller.songsData[0]["title"]
+            );
+          }
+        ),
       ),
     );
   }
